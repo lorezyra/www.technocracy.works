fb600ac5910641ad07024ba4888e5f1986ff6719
lorezyra <Rich@RichieBartlett.com>
2021-05-10 17:01:07 +0900

    New reload_cache λscript
    Completely refactored code. Made easier to read. Tested and updated permissions for logging & S3 triggers.
    
    Signed-off-by: lorezyra <Rich@RichieBartlett.com>

--- AWS/www_Technocracy_Works__CloudFront_invalidate/CloudFront_Invalidate-cache.js
+++ AWS/www_Technocracy_Works__CloudFront_invalidate/CloudFront_Invalidate-cache.js
@@ -3,43 +3,40 @@
 console.log('Loading event');
 
 var AWS = require('aws-sdk');
-AWS.config.apiVersions = {cloudfront: '2020-05-31'};
+AWS.config.apiVersions = { cloudfront: '2020-05-31' };
 var cloudFront = new AWS.CloudFront();
 
 
 
 exports.handler = async event => {
-    _log('Received event: ', event);
+    _log('Received event', event);
 
-    const s3BucketToCloudFrontDistributionId = {
-        //"S3 Bucket": "CloudFront Distribution ID",
-        "technocracy.works": "E3UQOMJO4R85Y9",
-        "richiebartlett-com": "E33GBX2OAB03UP"
-    };
-    // const distributionId = s3BucketToCloudFrontDistributionId[getS3BucketFromEvent(event)];
-_log("s3BucketToCloudFrontDistributionId", s3BucketToCloudFrontDistributionId[getS3BucketFromEvent(event)]);
-
-    const paths = getPathsFromEvent(event);
     const params = {
-        DistributionId:'E3UQOMJO4R85Y9',
+        DistributionId: getDistributionID(event),
         InvalidationBatch: {
             CallerReference: new Date().getTime().toString(),
-            Paths: paths
+            Paths: getPathsFromEvent(event)
         }
     };
 
     // @see https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/CloudFront.html#createInvalidation-property
     await cloudFront
-        .createInvalidation(params, function (err, data) {
-            if (err) {
-                console.log(err, err.stack);
-            } else {
-                console.log(data);
-            }
-        })
+        .createInvalidation(params, cfInvalidationCB)
         .promise();
-    _log("invalidate", paths.Items[0]);
+    // _log("invalidate", getPathsFromEvent(event).Items);
 };
+
+
+function getDistributionID(event) {
+    const s3BucketToCloudFrontDistributionId = {
+        //"S3 Bucket": "CloudFront Distribution ID",
+        "technocracy.works": "E3UQOMJO4R85Y9",
+        "richiebartlett-com": "E33GBX2OAB03UP"
+    };
+    // _log("s3BucketToCloudFrontDistributionId", s3BucketToCloudFrontDistributionId[getS3BucketFromEvent(event)]);
+
+    return s3BucketToCloudFrontDistributionId[getS3BucketFromEvent(event)];
+}
 
 function getPathsFromEvent(event) {
     console.log(JSON.stringify(event));
@@ -61,9 +58,24 @@ function getS3ObjectsFromEvent(event) {
     //     '*.jpg',
     //     '*.gif',
     //     '*.png',
-    //     '*.svg'
+    //     '*.ico',
+    //     '*.svg',
+    //     '*.txt',
+    //     '*.xml',
+    //     '*.json'
     // ]
+    // // .html .js .css .jpg .gif .png .ico .svg .txt .xml .json
     return event.Records[0].s3.object.key;
+}
+
+function cfInvalidationCB(err, data) {
+    /* callback function for cloudFront.createInvalidation(); */
+    if (err) {
+        console.log(err, err.stack);
+    }
+    else {
+        console.log(data);
+    }
 }
 
 function _log(caption, object) {
